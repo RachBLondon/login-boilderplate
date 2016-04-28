6d4833aa48b51b499137cdc1fbd9a687e84edfe1
Rachel <rachel.black@flashtalking.com>
2016-04-28 14:01:18 +0100

    form validated on client side

--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -4,10 +4,56 @@ import * as actions from '../../actions'
 
 class Signup extends Component {
   render(){
+    const { handleFormSubmit, fields:{ email, password, passwordConfirm}} = this.props;
     return (
-      <div> Sign Up </div>
+      <form>
+        <fieldset className="form-group">
+          <label> Email: </label>
+          <input className="form-control" { ...email } />
+          { email.touched && email.error && <div className="error">{email.error}</div> }
+        </fieldset>
+
+        <fieldset className="form-group">
+          <label> password: </label>
+          <input className="form-control" type="password" { ...password } />
+          {password.touched && password.error && <div className="error">{password.error}</div>}
+        </fieldset>
+
+        <fieldset className="form-group">
+          <label> Confirm Password: </label>
+          <input className="form-control" type="password" { ...passwordConfirm } />
+          { passwordConfirm.touched && passwordConfirm.error && <div className="error">{passwordConfirm.error}</div> }
+        </fieldset>
+        <button action="submit" className="btn btn-primary"> Sign Up </button>
+      </form>
     )
   }
 }
 
-export default Signup;
+function validate(formProps){
+  const errors = {};
+
+  // can be refactored
+  if(!formProps.email){
+    errors.email = 'Please enter an email';
+  }
+
+  if(!formProps.password){
+    errors.password = 'Please enter a password';
+  }
+
+  if(!formProps.passwordConfirm){
+    errors.passwordConfirm = 'Please confirm your password';
+  }
+
+  if(formProps.password !== formProps.passwordConfirm){
+    errors.password = 'Password must match';
+  }
+  return errors;
+}
+
+export default reduxForm({
+  form: 'signup',
+  fields: ['email', 'password', 'passwordConfirm'],
+  validate
+})(Signup);
