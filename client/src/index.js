e4da75b15ca3d1cf279d78b66f3c5a9ef206e5cb
Rachel <rachel.black@flashtalking.com>
2016-04-28 12:02:22 +0100

    sign up placeholder created

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,7 @@ import reduxThunk from 'redux-thunk';
 import App from './components/app';
 import Signin from './components/auth/Signin';
 import Signout from './components/auth/Signout';
+import Signup from './components/auth/Signup';
 
 import rootReducer from './reducers';
 import DevTools from './components/DevTools'
@@ -34,6 +35,7 @@ export default function configureStore(initialState){
         <Route path="/" component={App}>
           <Route path="signin" component={Signin}/>
           <Route path="signout" component={Signout}/>
+          <Route path="signup" component={Signup}/>
         </Route>
       </Router>
     </Provider>
