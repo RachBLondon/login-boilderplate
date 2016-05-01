9871595317e9babba1494ebd260a55d4f3236ad9
Rachel <rachel.black@flashtalking.com>
2016-05-01 19:57:55 +0100

    adding welcome and feature component route

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,14 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 import reduxThunk from 'redux-thunk';
+import RequireAuth from './components/auth/RequireAuth';
 
 import App from './components/app';
 import Signin from './components/auth/Signin';
 import Signout from './components/auth/Signout';
 import Signup from './components/auth/Signup';
+import Feature from './components/Feature';
+import Welcome from './components/Welcome';
 
 import rootReducer from './reducers';
 import DevTools from './components/DevTools'
@@ -33,9 +36,11 @@ export default function configureStore(initialState){
     <Provider store={store}>
       <Router history={browserHistory}>
         <Route path="/" component={App}>
+          <IndexRoute component={Welcome}/>
           <Route path="signin" component={Signin}/>
           <Route path="signout" component={Signout}/>
           <Route path="signup" component={Signup}/>
+          <Route path="feature" component={RequireAuth(Feature)} />
         </Route>
       </Router>
     </Provider>
